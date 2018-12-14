e78320b9dca9b2a2b1887ed409814c7d9b821dc8
Alexander <alejandroplatero29@gmail.com>
2018-12-14 19:41:27 +0100

    Anio bisiesto por teclado

---
+++ Ejercicio_25_Tema_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ejercicio_25_Tema_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anio = 0;
+            bool continuar = false;
+            
+            Console.Clear();
+            System.Console.Write("Introduzca un número: ");
+            do
+            {
+                try
+                {
+                    anio = int.Parse(Console.ReadLine());
+                    continuar = true;
+                }
+                catch (FormatException)
+                {
+                    System.Console.Write("Introduzca un número: ");
+                }
+                catch (OverflowException)
+                {
+                    System.Console.Write("Introduzca un número más pequeño: ");
+                }
+                catch (Exception)
+                {
+                    System.Console.WriteLine("Error genérico");
+                }
+
+            }while(continuar == false);
+
+            if(anio % 4 == 0 || anio % 100 == 0 && anio % 400 == 0)
+            {
+                System.Console.WriteLine("El año " + anio + " es bisiesto");
+            } 
+            else
+            {
+                System.Console.WriteLine("El año " + anio + " no es bisiesto");
+            }
+
+        }
+    }
+}
