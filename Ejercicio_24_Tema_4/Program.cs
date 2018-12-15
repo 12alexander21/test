0b1e1d336131e618c5ae68fce6841b42eb20772d
Alexander <alejandroplatero29@gmail.com>
2018-12-15 01:51:35 +0100

    new thing

---
+++ Ejercicio_24_Tema_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio_24_Tema_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pi=4;
+
+            for (int i = 3 ; i <= 1000; i+=2)
+            {
+                pi = pi + 4/i;
+                System.Console.Write(pi = pi + 4/i);
+                pi = pi - 4/i;
+                System.Console.Write(pi = pi - 4/i);
+            }
+        }
+    }
+}
